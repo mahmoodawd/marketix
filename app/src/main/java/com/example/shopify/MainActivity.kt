a9faf0dd059dbc1a7877518e05c8eb886b374b7a
AbdelrahmanEsam <abdelrahmanesam20000@gmail.com>
2023-07-17 04:42:48 +0300

    implementing design for checkout screen

--- app/src/main/java/com/example/shopify/MainActivity.kt
+++ app/src/main/java/com/example/shopify/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.example.shopify
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.example.shopify.databinding.ActivityMainBinding
 import com.example.shopify.utils.connectivity.ConnectivityObserver
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -24,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     @Inject
-    private lateinit var connectivityObserver: ConnectivityObserver
+    lateinit var connectivityObserver: ConnectivityObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
