4d7843dcab71525e25f142854dd4a041a5bf2749
AbdelrahmanEsam <abdelrahmanesam20000@gmail.com>
2023-07-18 11:03:42 +0300

    implementing design for details page screen

---
+++ app/src/main/java/com/example/shopify/productdetails/presentation/NewReviewFragment.kt
@@ -0,0 +1,24 @@
+package com.example.shopify.productdetails.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shopify.databinding.AddReviewLayoutBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class NewReviewFragment : BottomSheetDialogFragment() {
+    companion object {
+        const val TAG = "Tag"
+    }
+
+    private lateinit var binding: AddReviewLayoutBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = AddReviewLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
