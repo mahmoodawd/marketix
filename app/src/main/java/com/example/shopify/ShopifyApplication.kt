789cbe3afd06686227453a081c01ccff20ae21ac
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-07-18 11:50:12 +0300

    added timber for logging

--- app/src/main/java/com/example/shopify/ShopifyApplication.kt
+++ app/src/main/java/com/example/shopify/ShopifyApplication.kt
@@ -2,6 +2,12 @@ package com.example.shopify
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class ShopifyApplication : Application()
+class ShopifyApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
