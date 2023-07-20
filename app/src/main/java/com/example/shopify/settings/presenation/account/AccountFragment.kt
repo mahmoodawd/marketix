b248b88bc27f67f711bdb01fb74dea5282fdb1f4
AbdelrahmanEsam <abdelrahmanesam20000@gmail.com>
2023-07-20 03:11:36 +0300

    fixing nested graph issues and implementing address feature

--- app/src/main/java/com/example/shopify/settings/presenation/account/AccountFragment.kt
+++ app/src/main/java/com/example/shopify/settings/presenation/account/AccountFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.shopify.R
 import com.example.shopify.databinding.FragmentAccountBinding
 import com.example.shopify.databinding.FragmentAddressBinding
@@ -16,7 +18,7 @@ class AccountFragment : Fragment() {
 
     private lateinit var binding: FragmentAccountBinding
 
-
+private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +29,7 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
     }
 
 
